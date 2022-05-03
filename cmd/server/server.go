8eae0e977fcd2c86c093ed6a7600194bb8f1ee6e
o-t-k-t <wetcondition@gmail.com>
2022-05-03 21:57:13 +0900

    move server setup process into infra.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/o-t-k-t/graphl_app_trial/graph"
-	"github.com/o-t-k-t/graphl_app_trial/graph/generated"
-)
-
-const defaultPort = "8080"
+import "github.com/o-t-k-t/graphl_app_trial/app/infrastructure/server"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	server.SetupServer()
 }
